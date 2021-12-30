bdd7f3d93b34638d0c89fb7685a5aa69b77e6d60
Ewe Tek Min <ewe.tekmin@outlook.com>
2021-12-30 21:49:03 +0000

    Add styles

--- src/App.js
+++ src/App.js
@@ -4,23 +4,39 @@ const App = () => {
   const [selectedLanguage, setSelectedLanguage] = useState('en');
   const handleLanguageSelect = (event) => {
     setSelectedLanguage(event.target.value);
-  }
+  };
 
   return (
-    <div>
-      <label>
-        English
-        <input type="radio" value="en" checked={selectedLanguage === 'en'} onClick={handleLanguageSelect} />
-      </label>
-      <label>
-        中文
-        <input type="radio" value="zh" checked={selectedLanguage === 'zh'} onClick={handleLanguageSelect} />
-      </label>
-      <p>Your selected language is: {selectedLanguage} </p>
-      <p>Your have switch language for 0 times</p>
-      <p>Welcome to the world of wonder</p>
+    <div className="h-screen flex justify-center items-center">
+      <div className="mx-auto bg-white p-4 rounded space-y-2">
+        <div className='space-x-4'>
+          <label>
+            English
+            <input
+              type="radio"
+              value="en"
+              checked={selectedLanguage === 'en'}
+              onClick={handleLanguageSelect}
+              className='ml-2'
+            />
+          </label>
+          <label>
+            中文 (Chinese)
+            <input
+              type="radio"
+              value="zh"
+              checked={selectedLanguage === 'zh'}
+              onClick={handleLanguageSelect}
+              className='ml-2'
+            />
+          </label>
+        </div>
+        <p>Your selected language is: {selectedLanguage} </p>
+        <p>Your have switch language for 0 times</p>
+        <p>Welcome to the world of wonder</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
