11717095248235156d82faefaf3f0e9273c7af8c
Ewe Tek Min <ewe.tekmin@outlook.com>
2022-01-01 22:17:37 +0000

    Update implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import i18next from 'i18next';
 
 import './i18n.config';
 
 const App = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
   const [count, setCount] = useState(0);
-  const [selectedLanguage, setSelectedLanguage] = useState('en');
+
   const handleLanguageSelect = (event) => {
     setSelectedLanguage(event.target.value);
-    i18next.changeLanguage(event.target.value);
-    setCount(count + 1);
+    i18n.changeLanguage(event.target.value);
+    setCount(count => count + 1);
   };
 
   return (
@@ -23,7 +23,7 @@ const App = () => {
             <input
               type="radio"
               value="en"
-              checked={selectedLanguage === 'en'}
+              checked={selectedLanguage.includes('en')}
               onClick={handleLanguageSelect}
               className="ml-2"
             />
@@ -33,16 +33,16 @@ const App = () => {
             <input
               type="radio"
               value="zh"
-              checked={selectedLanguage === 'zh'}
+              checked={selectedLanguage.includes('zh')}
               onClick={handleLanguageSelect}
               className="ml-2"
             />
           </label>
         </div>
         <section>
+          <p>{t('welcome')}</p>
           <p>{t('selectedLanguage', { language: selectedLanguage })}</p>
           <p>{t('numOfTimesSwitchingLanguage', { count })}</p>
-          <p>{t('welcome')}</p>
         </section>
       </div>
     </div>
--- src/i18n.config.js
+++ src/i18n.config.js
@@ -1,11 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
 import en from './resources/en.json';
 import zh from './resources/zh.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources: {
       en: {
